d1d39f9bae708439fcb7bf6f8c7ab9125c7a8dde
Khagendra Lama <khagendralama2@gmail.com>
2024-12-20 16:12:03 +0545

    Colored clock with setInterval

--- test.js
+++ test.js
@@ -1,63 +1,25 @@
-function createCalendar(elem, year, month) {
-    // debugger;
-    let table = document.createElement('table');
-    let date = new Date(year, month - 1);
-    let days = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
+document.body.insertAdjacentHTML('afterbegin', '<div class="timer"><span class="hr">hr</span>:<span class="min">mm</span>:<span class="sec">ss</span></div>');
 
-    // Create heading
-    let headingRow = document.createElement('tr');
-    for(let i = 0; i < days.length; i++) {
-        let heading = document.createElement('th');
-        heading.textContent = days[i];
-        headingRow.append(heading);
-    }
-    table.append(headingRow);
+let timerId;
 
-    // Create calendar days
-    let monthEnd = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    let lastDay = monthEnd.getDate();
-    let createRow = true;
-    let dataRow;
-    let beforeGaps;
-    let afterGaps;
-    let totalCells;
-    let day = 1;
-
-    if(date.getDay() == 0) {
-        beforeGaps = 6;
-    } else if(date.getDay() == 1) {
-        beforeGaps = 0;
-    } else {
-        beforeGaps = date.getDay() - 1;
+function clockStart() {
+    if(!timerId) {
+        timerId = setInterval(update, 1000);
     }
 
-    if(monthEnd.getDay() == 0) {
-        afterGaps = 0;
-    } else {
-        afterGaps = 7 - monthEnd.getDay();
-    }
-
-    totalCells = beforeGaps + lastDay + afterGaps;
-
-    for(let i = 1; i <= totalCells; i++) {
-        if(createRow) {
-            dataRow = document.createElement('tr');
-            createRow = false;
-        }
-
-        let data = document.createElement('td');
-        data.textContent = i <= beforeGaps || i > beforeGaps + lastDay ? '' : day++;
-        dataRow.append(data);
-
-        // Append the data row to the table
-        if(i%7 == 0) {
-            table.append(dataRow);
-            createRow = true;
-        }
-    }
-    
-    // Append table to the container
-    elem.append(table);
+    update();
 }
 
-createCalendar(calendar, 2001, 9);
+function clockStop() {
+    clearInterval(timerId);
+}
+
+function update() {
+    let d = new Date();
+
+    document.querySelector('.hr').textContent = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+
+    document.querySelector('.min').textContent = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+
+    document.querySelector('.sec').textContent = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+}
