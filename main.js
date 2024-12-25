3fbd265befca0c8111f47fa8a3cc7c1c2a0b6465
Khagendra Lama <khagendralama2@gmail.com>
2024-12-25 10:38:32 +0545

    Find window coordinates of the field

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-let ball = document.getElementById('ball');
+document.onclick = function(e) {
+    coords.innerHTML = e.clientX + ':' + e.clientY;
+};
 
-ball.style.left = (ball.offsetParent.scrollWidth / 2) - (ball.clientWidth / 2) + 'px';
-ball.style.top = (ball.offsetParent.scrollHeight / 2) - (ball.clientHeight / 2) + 'px';
+let field = document.getElementById('field');
+let fieldCoords = field.getBoundingClientRect();
+console.log(fieldCoords);
+
+// Coordinates
+let outerUpperLeft = `Outer upper-left: ${fieldCoords.left}, ${fieldCoords.top}`;
+
+let outerBottomRight = `Outer bottom-right: ${fieldCoords.left + fieldCoords.width}, ${fieldCoords.top + fieldCoords.height}`;
+
+let innerUpperLeft = `Inner upper-left: ${field.offsetLeft + field.clientLeft}, ${field.offsetTop + field.clientTop}`;
+
+let innerBottomRight = `Inner bottom-right: ${field.offsetLeft + field.clientLeft + field.clientWidth}, ${field.offsetTop + field.clientTop + field.clientHeight}`;
+
+// Log the coordinates
+console.log(outerUpperLeft);
+console.log(outerBottomRight);
+console.log(innerUpperLeft);
+console.log(innerBottomRight);
