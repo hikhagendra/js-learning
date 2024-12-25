dcc75bc7fefe6b3393e7fe0c8d715b5fd13a79d5
Khagendra Lama <khagendralama2@gmail.com>
2024-12-25 11:52:18 +0545

    Show a note near the element

--- main.js
+++ main.js
@@ -1,22 +1,69 @@
-document.onclick = function(e) {
-    coords.innerHTML = e.clientX + ':' + e.clientY;
-};
+/**
+ * Positions elem relative to anchor as said in position.
+ *
+ * @param {Node} anchor     Anchor element for positioning
+ * @param {string} position One of: top/right/bottom
+ * @param {Node} elem       Element to position
+ *
+ * Both elements: elem and anchor must be in the document
+*/
+function positionAt(anchor, position, elem) {
+    let ancCoords = anchor.getBoundingClientRect();
+    let elemCoords = elem.getBoundingClientRect();
 
-let field = document.getElementById('field');
-let fieldCoords = field.getBoundingClientRect();
-console.log(fieldCoords);
+    switch (position) {
+        case 'top':
+            elem.style.top = ancCoords.top - elemCoords.height + 'px';
+            elem.style.left = ancCoords.left + 'px';
+            break;
+    
+        case 'right':
+            elem.style.top = ancCoords.top + 'px';
+            elem.style.left = ancCoords.left + ancCoords.width + 'px';
+            break;
 
-// Coordinates
-let outerUpperLeft = `Outer upper-left: ${fieldCoords.left}, ${fieldCoords.top}`;
+        case 'bottom':
+            elem.style.top = ancCoords.top + ancCoords.height + 'px';
+            elem.style.left = ancCoords.left + 'px';
+            break;
+        
+        case 'top-in':
+            elem.style.top = ancCoords.top + 'px';
+            elem.style.left = ancCoords.left + 'px';
+            break;
 
-let outerBottomRight = `Outer bottom-right: ${fieldCoords.left + fieldCoords.width}, ${fieldCoords.top + fieldCoords.height}`;
+        case 'bottom-in':
+            elem.style.top = ancCoords.top + ancCoords.height - elemCoords.height + 'px';
+            elem.style.left = ancCoords.left + 'px';
+            break;
 
-let innerUpperLeft = `Inner upper-left: ${field.offsetLeft + field.clientLeft}, ${field.offsetTop + field.clientTop}`;
+        case 'right-in':
+            elem.style.top = ancCoords.top + 'px';
+            elem.style.left = ancCoords.left + ancCoords.width - elemCoords.width + 'px';
 
-let innerBottomRight = `Inner bottom-right: ${field.offsetLeft + field.clientLeft + field.clientWidth}, ${field.offsetTop + field.clientTop + field.clientHeight}`;
+    }
+}
 
-// Log the coordinates
-console.log(outerUpperLeft);
-console.log(outerBottomRight);
-console.log(innerUpperLeft);
-console.log(innerBottomRight);
+/**
+ * Shows a note with the given html at the given position
+ * relative to the anchor element.
+ */
+function showNote(anchor, position, html) {
+
+    let note = document.createElement('div');
+    note.className = "note";
+    note.innerHTML = html;
+    document.body.append(note);
+
+    positionAt(anchor, position, note);
+}
+
+// test it
+let blockquote = document.querySelector('blockquote');
+
+showNote(blockquote, "top", "note above");
+showNote(blockquote, "right", "note at the right");
+showNote(blockquote, "bottom", "note below");
+showNote(blockquote, "bottom-in", "note bottom-in");
+showNote(blockquote, "top-in", "note top-in");
+showNote(blockquote, "right-in", "note right-in");
