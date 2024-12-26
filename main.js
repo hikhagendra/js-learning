ef27ab204767899eb04b153f05e20e758f6fb272
Khagendra Lama <khagendralama2@gmail.com>
2024-12-26 13:46:01 +0545

    Add a clsing button

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-let toggle = document.getElementById('toggle');
-let menu = document.getElementById('menu');
+let messages = document.getElementsByClassName('pane');
 
-toggle.addEventListener('click', function() {
-    toggle.classList.toggle('close');
-    menu.classList.toggle('hide');
-});
+// Add the close button to all messages
+for(let msg of messages) {
+    // Create a close button
+    let closeBtn = document.createElement('button');
+
+    // Add button class and content
+    closeBtn.classList.add('remove-button');
+    closeBtn.textContent = '[x]';
+
+    // Append the button to message box
+    msg.append(closeBtn);
+
+    // Get the message and button's coordinates
+    let msgCoords = msg.getBoundingClientRect();
+    let btnCoords = closeBtn.getBoundingClientRect();
+    
+    // Position button to the top right of the message box
+    closeBtn.style.top = '0px';
+    closeBtn.style.left = msgCoords.width - btnCoords.width - 10 + 'px';
+}
+
+let closeBtns = document.getElementsByClassName('remove-button');
+
+for(let btn of closeBtns) {
+    btn.addEventListener('click', function(e) {
+        e.currentTarget.parentNode.remove();
+    });
+}
