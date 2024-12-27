16dfbb68f1b90b1369959708f3ee6abee7ddce92
Khagendra Lama <khagendralama2@gmail.com>
2024-12-27 15:57:30 +0545

    Tree menu

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-let container = document.getElementById('container')
+let tree = document.getElementById('tree');
+let lis = document.querySelectorAll('li');
 
-container.addEventListener('click', function(event) {
-    let remove = event.target.classList.contains('remove-button');
+for(let li of lis) {
+    let span = document.createElement('span');
+    li.prepend(span);
+    span.append(span.nextSibling);
+}
 
-    if(remove) {
-        event.target.closest('.pane').remove();
+tree.addEventListener('click', function(event) {
+    let elem = event.target;
+
+    if(elem.tagName !== 'SPAN') return;
+
+    let li = elem.closest('li');
+    let hasChildren = li.children.item(1);
+
+    if(li && hasChildren) {
+        li.children.item(1).toggleAttribute('hidden');
     }
 });
