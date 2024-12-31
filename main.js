e0ee30574d5ea5c8a652e66a1e827b50e396af02
Khagendra Lama <khagendralama2@gmail.com>
2024-12-31 13:02:51 +0545

    Catch links in the element

--- main.js
+++ main.js
@@ -1,54 +1,11 @@
-document.addEventListener('mouseover', function(event) {
-    let hasToolTip = event.target.hasAttribute('data-tooltip');
+let content = document.getElementById('contents');
 
-    if(hasToolTip) {
-        showToolTip(event.target);
-    }
+content.addEventListener('click', function(event) {
+    let target = event.target.closest('a');
+
+    if(!target) return;
+
+    let ask = confirm(`Leave for ${target.href}`);
+
+    if(!ask) event.preventDefault();
 });
-
-document.addEventListener('mouseout', function(event) {
-    let hasToolTip = event.target.hasAttribute('data-tooltip');
-
-    if(hasToolTip) {
-        hideToolTip(event.target);
-    }
-});
-
-function showToolTip(elem) {
-    let content = elem.dataset.tooltip;
-
-    // Create tooltip element
-    let tooltip = document.createElement('span');
-    tooltip.innerHTML = content;
-    tooltip.classList.add('tooltip');
-    elem.append(tooltip);
-
-    // Get Element and Tooltip coordinates
-    let toolTipCoords = tooltip.getBoundingClientRect();
-    let elemCoords = elem.getBoundingClientRect();
-
-    // Tooltip Positioning
-    let leftPos = 0;
-    let topPos = 0;
-
-    if((elemCoords.left + (elemCoords.width / 2)) < (toolTipCoords.width / 2)) {
-        leftPos = elemCoords.left;
-    } else {
-        leftPos = elemCoords.left + ((elemCoords.width - toolTipCoords.width)/2);
-    }
-
-    if((elemCoords.top - 5) < toolTipCoords.height) {
-        topPos = elemCoords.top + elemCoords.height + 5;
-    } else {
-        topPos = elemCoords.top - toolTipCoords.height - 5;
-    }
-
-    tooltip.style.left = leftPos + 'px';
-    tooltip.style.top = topPos + 'px';
-}
-
-function hideToolTip(elem) {
-    if(elem.lastChild.classList.contains('tooltip')) {
-        elem.lastChild.remove();
-    }
-}
