ddcfba73db596e89a5dc0af0e9c45c4e03451c12
Khagendra Lama <khagendralama2@gmail.com>
2024-12-31 17:00:08 +0545

    Selectable list

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-let screen = document.getElementById('largeImg');
-let thumbs = document.getElementById('thumbs');
+let list = document.getElementById('ul');
 
-thumbs.addEventListener('click', function(event) {
-    if(event.target.tagName != 'IMG') return;
+// Disable text selection
+list.onmousedown = function() { return false; }
 
-    let lgImg = event.target.closest('a');
+list.addEventListener('click', function(event) {
+    if(event.target.tagName != 'LI') return;
 
-    screen.setAttribute('src', lgImg.getAttribute('href'));
-    screen.setAttribute('alt', lgImg.getAttribute('title'));
-
-    event.preventDefault();
+    if(event.ctrlKey || event.metaKey) {
+        event.target.classList.toggle('selected');
+    } else {
+        disSelectOld();
+        event.target.classList.add('selected');
+    }
 });
+
+function disSelectOld() {
+    for(let item of list.querySelectorAll('li')) {
+        item.classList.remove('selected');
+    }
+}
