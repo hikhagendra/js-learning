a2a0312f3eaab78cfe6225b7ec490a52f88de1c4
Khagendra Lama <khagendralama2@gmail.com>
2025-01-08 17:53:55 +0545

    Slider

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-new HoverIntent({
-    elem,
-    over() {
-      tooltip.style.left = elem.getBoundingClientRect().left + 5 + 'px';
-      tooltip.style.top = elem.getBoundingClientRect().bottom + 5 + 'px';
-      tooltip.hidden = false;
-    },
-    out() {
-      tooltip.hidden = true;
+let slider = document.getElementById('slider');
+let thumb = document.getElementById('thumb');
+
+thumb.addEventListener('mousedown', function(event) {
+  event.preventDefault();
+
+  let sliderCoords = slider.getBoundingClientRect();
+  let thumbCoords = thumb.getBoundingClientRect();
+  let shiftX = event.clientX - thumbCoords.left;
+
+  moveAt(event.clientX, event);
+
+  function moveAt(pageX, event) {
+    if(event.clientX < sliderCoords.left) {
+      pageX = sliderCoords.left;
+    } else if(event.clientX > sliderCoords.left + sliderCoords.width - thumbCoords.width) {
+      pageX = sliderCoords.left + sliderCoords.width - thumbCoords.width;
+    } else {
+      pageX = event.clientX;
     }
+
+    thumb.style.left = pageX - sliderCoords.left - shiftX + 'px';
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  function onMouseMove(event) {
+    moveAt(event.clientX, event);
+
+    document.onmouseup = thumb.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.onmouseup = thumb.onmouseup = null;
+    };
+  }
 });
