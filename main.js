29974e6631a679a85e79b4304087924c49227316
Khagendra Lama <khagendralama2@gmail.com>
2025-01-09 22:10:48 +0545

    Webhook setup test

--- main.js
+++ main.js
@@ -1,38 +1,7 @@
-let slider = document.getElementById('slider');
-let thumb = document.getElementById('thumb');
-
-thumb.addEventListener('pointerdown', function(event) {
+document.addEventListener('keydown', function(event) {
   event.preventDefault();
-  thumb.setPointerCapture(event.pointerId);
-
-  let sliderCoords = slider.getBoundingClientRect();
-  let thumbCoords = thumb.getBoundingClientRect();
-  let shiftX = event.clientX - thumbCoords.left;
-
-  moveAt(event.clientX, event);
-
-  function moveAt(pageX, event) {
-    if(event.clientX < sliderCoords.left) {
-      pageX = sliderCoords.left + shiftX;
-    } else if(event.clientX > sliderCoords.left + sliderCoords.width - thumbCoords.width) {
-      pageX = sliderCoords.left + sliderCoords.width - thumbCoords.width + shiftX;
-    } else {
-      pageX = event.clientX;
-    }
-
-    console.log(pageX);
-
-    thumb.style.left = pageX - sliderCoords.left - shiftX + 'px';
+  console.log('Started repeating');
+  if(event.repeat) {
+    alert('Wait wait');
   }
-
-  thumb.addEventListener('pointermove', onMouseMove);
-
-  function onMouseMove(event) {
-    moveAt(event.clientX, event);
-
-    thumb.onmouseup = thumb.onmouseup = function() {
-      thumb.removeEventListener('pointermove', onMouseMove);
-      thumb.onmouseup = thumb.onmouseup = null;
-    };
-  }
-});
+})
