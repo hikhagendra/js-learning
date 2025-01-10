8a6797166d83edb66dd61336a4d600f6ea8ad8ba
Khagendra Lama <khagendralama2@gmail.com>
2025-01-10 15:27:43 +0545

    Up/down button

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-window.addEventListener('scroll', function() {
-  if((document.documentElement.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight)) <= 100) {
-    insertDate(1);
-  }
-});
+let arrow = document.getElementById('arrowTop');
 
-function insertDate(counter) {
-  for(let i = 0; i < counter; i++) {
-    let date = new Date();
-    let p = document.createElement('p');
-    p.textContent = `Date: ${date}`;
-
-    document.body.append(p);
-  }
+document.onscroll = function() {
+  arrowVisibility();
 }
 
-insertDate(20);
+arrowVisibility();
+
+arrow.addEventListener('click', function(event) {
+  window.scrollTo(0, 0);
+});
+
+function arrowVisibility() {
+  let offsetTop = window.pageYOffset;
+  let screenHeight = document.documentElement.clientHeight;
+
+  if(offsetTop >= screenHeight) {
+    arrow.hidden = false;
+  } else {
+    arrow.hidden = true;
+  }
+}
