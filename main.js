f8e9fe15f590339193478bbd335fccd6aa17b885
Khagendra Lama <khagendralama2@gmail.com>
2025-01-10 12:42:28 +0545

    Extended hotkeys

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-document.addEventListener('keydown', function(event) {
-  event.preventDefault();
-  console.log('Started repeating');
-  if(event.repeat) {
-    alert('Wait wait');
-  }
-})
+function runOnKeys(func, ...rest) {
+  let args = rest;
+  let trackedKeys = [];
+  
+  document.addEventListener('keydown', function(event) {
+    if(!args.includes(event.code)) return;
+
+    if(args.includes(event.code) && !trackedKeys.includes(event.code)) {
+      trackedKeys.push(event.code);
+    }
+
+    if(trackedKeys.length == args.length) {
+      func();
+      trackedKeys = [];
+    }
+
+    document.onkeyup = function(event) {
+      if(trackedKeys.includes(event.code)) {
+        let pos = trackedKeys.findIndex(item => item == event.code);
+
+        trackedKeys.splice(pos, 1);
+      }
+    }
+  });
+}
+
+runOnKeys(
+  () => alert('Hello!'),
+  'KeyQ',
+  'KeyW',
+);
