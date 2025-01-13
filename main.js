018ef887d5ed6f6a9a88fe1e0ee52cb9ea194724
Khagendra Lama <khagendralama2@gmail.com>
2025-01-13 11:36:52 +0545

    Load visible images with preload functionality

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-let arrow = document.getElementById('arrowTop');
-
-document.onscroll = function() {
-  arrowVisibility();
-}
-
-arrowVisibility();
-
-arrow.addEventListener('click', function(event) {
-  window.scrollTo(0, 0);
-});
-
-function arrowVisibility() {
-  let offsetTop = window.pageYOffset;
-  let screenHeight = document.documentElement.clientHeight;
-
-  if(offsetTop >= screenHeight) {
-    arrow.hidden = false;
+/**
+ * Tests if the element is visible (within the visible part of the page)
+ * It's enough that the top or bottom edge of the element are visible
+ */
+function isVisible(elem) {
+  let elemCoords = elem.getBoundingClientRect();
+  let from = window.pageYOffset;
+  let to = window.pageYOffset + (document.documentElement.clientHeight * 2);
+  let offsetBottom = elem.offsetTop + elem.offsetHeight;
+  let onScreen = (elem.offsetTop > from && elem.offsetTop < to) || (offsetBottom > from && offsetBottom < to);
+  
+  if(onScreen) {
+    return true;
   } else {
-    arrow.hidden = true;
+    return false;
   }
 }
+
+function showVisible() {
+  for (let img of document.querySelectorAll('img')) {
+    let realSrc = img.dataset.src;
+    if (!realSrc) continue;
+
+    if (isVisible(img)) {
+      realSrc += '?nocache=' + Math.random();
+
+      img.src = realSrc;
+
+      img.dataset.src = '';
+    }
+  }
+}
+
+window.addEventListener('scroll', showVisible);
+showVisible();
