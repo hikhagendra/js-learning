768140c5242a085f6a6950aee3673a1cf382d8e7
Khagendra Lama <khagendralama2@gmail.com>
2025-01-14 10:55:32 +0545

    Edit TD on click

--- main.js
+++ main.js
@@ -1,32 +1,83 @@
-let view = document.getElementById('view');
-let editor;
+let table = document.getElementById('bagua-table');
+let editor = null;
+let cell = null;
+let ok = null;
+let cancel = null;
 
-view.addEventListener('click', function(event) {
-  editorSwitch('edit', event.target);
+table.addEventListener('click', editTrigger);
 
-  editor.addEventListener('focusout', function(event) {
-    editorSwitch('view', null)
-  });
-});
-
-window.addEventListener('keydown', function(event) {
-  if(event.key == 'Enter') editorSwitch('view', null);
-});
-
-function editorSwitch(mode, elem) {
-  switch(mode) {
-    case 'edit':
-      editor = document.createElement('textarea');
-      editor.classList.add('edit');
-      editor.value = elem.innerHTML;
-      view.replaceWith(editor);
-      editor.focus();
-      break;
-    
-    case 'view':
-      editor.blur();
-      view.innerHTML = editor.value;
-      editor.replaceWith(view);
-      break;
+function editTrigger(event) {
+  if(event.target.tagName == 'TD') {
+    cell = event.target;
+    editCell(event.target);
   }
+}
+
+function editCell(cell) {
+  // Disable event
+  table.removeEventListener('click', editTrigger);
+
+  // Initialize editor
+  let cellCoords = cell.getBoundingClientRect();
+  editor = document.createElement('textarea');
+
+  // Match geometry
+  editor.style.width = cellCoords.width + 'px';
+  editor.style.height = cellCoords.height - 4 + 'px';
+  editor.classList.add('editor');
+
+  // Add content and replace
+  editor.value = cell.innerHTML;
+  cell.replaceWith(editor);
+  editor.focus();
+
+  // Set focus to the end
+  let length = editor.value.length;
+  editor.setSelectionRange(length, length);
+
+  // Add buttons
+  ok = document.createElement('button');
+  cancel = document.createElement('button');
+
+  // Add classes
+  ok.classList.add('ok');
+  cancel.classList.add('cancel');
+
+  // Add content
+  ok.textContent = 'OK';
+  cancel.textContent = 'Cancel';
+
+  // Append buttons
+  editor.after(ok);
+  editor.after(cancel);
+
+  // Position buttons in the editor
+  ok.style.top = cancel.style.top = cellCoords.top + cellCoords.height + 'px';
+  ok.style.left = cellCoords.left + 'px'
+  cancel.style.left = cellCoords.left + ok.getBoundingClientRect().width + 'px'
+
+  // Events on buttons
+  ok.addEventListener('click', saveUpdatedData);
+  cancel.addEventListener('click', closeEditor);
+}
+
+function saveUpdatedData() {
+  ok.remove();
+  cancel.remove();
+  editor.blur();
+  cell.innerHTML = editor.value;
+  editor.replaceWith(cell);
+
+  // Make other cells clickable
+  table.addEventListener('click', editTrigger);
+}
+
+function closeEditor() {
+  ok.remove();
+  cancel.remove();
+  editor.blur();
+  editor.replaceWith(cell);
+
+  // Make other cells clickable
+  table.addEventListener('click', editTrigger);
 }
