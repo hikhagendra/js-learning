0c9cee992b45caaaa30197307a3151cd76f54d3f
Khagendra Lama <khagendralama2@gmail.com>
2025-01-14 15:58:13 +0545

    Modal form

--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-// initial: the initial money sum
-// interest: e.g. 0.05 means 5% per year
-// years: how many years to wait
+let triggerPrompt = document.getElementById('show-prompt');
+let modal = document.getElementById('prompt-form-container');
 
-let form = document.forms.calculator;
-let moneyBefore = document.getElementById('money-before');
-let moneyAfter = document.getElementById('money-after');
-let diagramAfter = document.getElementById('height-after');
+function showPrompt(html, callback) {
+  function closePrompt(value) {
+    modal.hidden = true;
+    field.value = '';
+    field.blur();
+    firstElem.blur();
+    lastElem.blur();
+    callback(value);
+    document.body.style.overflow = '';
+    document.onkeydown = null;
+  }
 
-form.addEventListener('input', function(event) {
-  updateResult();
-});
+  // Show the modal
+  document.body.style.overflow = 'hidden';
+  modal.hidden = false;
 
-function updateResult() {
-  let initial = form.elements.money.value < 0 ? 0 : form.elements.money.value;
-  let interest = (form.elements.interest.value < 0) ? 0 : form.elements.interest.value / 100;
-  let years = form.elements.months.value / 12;
-  let result = Math.round(initial * (1 + interest) ** years);
+  // Variables initialization
+  let form = document.getElementById('prompt-form');
+  let msgBar = document.getElementById('prompt-message');
+  let field = form.elements.text;
+  let cancel = form.elements.cancel;
 
-  // Update data
-  moneyBefore.textContent = initial;
-  moneyAfter.textContent = result;
+  // Update content
+  msgBar.innerHTML = html;
 
-  // Update diagram
-  diagramAfter.style.height = (result / initial) * 100 + 'px';
+  // On submit
+  form.onsubmit = function(event) {
+    event.preventDefault();
+
+    if(field.value == '') return false;
+
+    closePrompt(field.value);
+  };
+
+  // On cancel
+  cancel.onclick = function() {
+    closePrompt(null);
+  };
+
+  // On key presses
+  document.onkeydown = function(event) {
+    if(event.key == 'Escape') {
+      closePrompt(null);
+    }
+  };
+
+  let lastElem = form.elements[form.elements.length - 1];
+  let firstElem = form.elements[0];
+
+  lastElem.onkeydown = function(e) {
+    if(e.key == 'Tab' && !e.shiftKey) {
+      firstElem.focus();
+      return false;
+    }
+  };
+
+  firstElem.onkeydown = function(e) {
+    if(e.key == 'Tab' && e.shiftKey) {
+      console.log('Now');
+      lastElem.focus();
+      return false;
+    }
+  };
+
+  // Set focus
+  field.focus();
 }
 
-updateResult();
+triggerPrompt.onclick = function() {
+  showPrompt("Welcome to <br>modal world :)", function(value) {
+    alert('You entered: ' + value);
+  });
+};
